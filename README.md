ac960f246405f00ea303aa85f95e7b729f5e5797
Marcin Krawczyk <marckraw@icloud.com>
2020-04-21 23:00:13 +0200

    Make plugin boilerplate out of plugin

--- README.md
+++ README.md
@@ -1,63 +1,53 @@
-# Storyblok Component Fade plugin
+# Storyblok Plugin Boilerplate compatible with sb-mig
 
-## Installing the plugin in your storyblok space 
-To use Component Fade plugin you need to compile and minify the plugin. But first you need to change name of the plugin.
+## Installing the plugin in your storyblok space
+
+To use `xxx` plugin you need to compile and minify the plugin. But first you need to change name of the plugin.
 Go to `./src/` and open `Plugin.vue`.
 You need to prefix plugin name, with for example name of your department:
+
 ```
 initWith() {
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: "department_name-component-fade"
+        plugin: "name-of-the-plugin"
       };
     },
 ```
 
-
 #### Compiles and minifies for production
+
 Go to root folder and run:
+
 ```
 npm install
 npm run build
 ```
 
-After running the build you should get a folder `/dist` with an `export.js` file inside. 
-Create a new plugin in storyblok (it has to have exact name as in first step) and paste the content of `export.js` and publish it.  
-The plugin will be available in your storyblok component schemas. 
+After running the build you should get a folder `/dist` with an `export.js` file inside.
+Create a new plugin in storyblok (it has to have exact name as in first step) and paste the content of `export.js` and publish it.
 
+<!-- TODO: you can also run `sb-mig plugin upload` command - not existing for now -->
 
-##### More info on storyblok plugin usage/implementation: 
+The plugin will be available in your storyblok component schemas.
+
+##### More info on storyblok plugin usage/implementation:
+
 https://www.storyblok.com/docs/Guides/Creating-a-field-type-plugin
 
 ---
 
 ## Using the plugin output in your components
+
 The output of the plugin has following structure:
 
 ```
 {
-  "plugin": "component-fade-plugin",
-  "classNames": {
-    "before": "before-fade",
-    "after": "after-fade"
-  },
-  "transitionConfig": {
-    "duration": 500,
-    "transitionProperty": "opacity",
-    "transitionTimingFunction": "ease"
-  },
-  "config": {
-    "threshold": "0.4"
-  },
-  "type": "classnames",
-  "effect": "fade",
-  "_uid": "78dba965-6c4e-42cc-9eb8-f04a50c8215e"
+  "plugin": "name-of-the-plugin",
+  "data": "some data",
+  "_uid": "78dba965-6c4e-42cc-9eb8-f04a50c8215e",
+  ...
+  ...
+  ...
 }
 ```
-* `classNames.before` - className which will be applied before intersection observer will be triggered (it is always before-*), where * is set up on plugin consumer
-* `classNames.after` - className which will be applied after inntersection observer will be triggered (it is always after-*), where * is set up on plugin consumer
-* `transitionConfig` - this is configuration object fot transition
-* `config` - this is configuration object which is passed to `IntersectionObserver`
-* `type` - type of animation supoported. Right now it's only transition with classnames (roadmap: support js animations)
-* `effect` - effect for the transition, this is mostly to for `classNames.before` and `after`
-
--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,119 +1,6 @@
 <template>
   <div class="container">
-    <div class="uk-margin">
-      <label
-        ><input v-model="model.enabled" class="uk-checkbox" type="checkbox" />
-        Enable transition</label
-      >
-    </div>
-    <template v-if="model.enabled">
-      <h5 class="transition-settings__heading">Transition settings</h5>
-      <div class="transition-settings">
-        <div class="uk-margin">
-          <div>
-            <label for="duration">Duration (ms)</label>
-          </div>
-          <input
-            v-model="model.transitionConfig.duration"
-            class="uk-input"
-            type="number"
-            id="duration"
-            name="duration"
-          />
-        </div>
-        <div class="uk-margin">
-          <div>
-            <label for="delay">Delay (ms)</label>
-          </div>
-          <input
-            v-model="model.transitionConfig.delay"
-            class="uk-input"
-            type="number"
-            id="delay"
-            name="delay"
-          />
-        </div>
-        <div class="uk-margin">
-              <label
-                ><input
-                  v-model="model.enabledOnMobile"
-                  class="uk-checkbox"
-                  type="checkbox"
-                  checked
-                />
-                Enabled on mobile</label
-              >
-            </div>
-        <div>
-          <button class="uk-button" @click="toggleMoreOptions">
-            {{ moreOptions ? "Less options..." : "More options..." }}
-          </button>
-        </div>
-        <template v-if="moreOptions">
-          <div class="uk-margin">
-            <div>
-              <label for="transitionTimingFunction"
-                >Transition timing function</label
-              >
-            </div>
-            <select
-              class="uk-select"
-              v-model="model.transitionConfig.transitionTimingFunction"
-              id="transitionTimingFunction"
-              name="transitionTimingFunction"
-            >
-              <option value="ease">ease</option>
-              <option>ease-in</option>
-              <option>ease-out</option>
-              <option>ease-in-out</option>
-              <option>linear</option>
-            </select>
-          </div>
-          <div class="uk-margin">
-            <div>
-              <label for="transitionProperty">Transition property</label>
-            </div>
-            <input
-              disabled
-              v-model="model.transitionConfig.transitionProperty"
-              class="uk-input"
-              type="text"
-              id="transitionProperty"
-              name="transitionProperty"
-            />
-          </div>
-          <h5 class="intersection-observer-settings__heading">
-            Intersection observer settings
-          </h5>
-          <div class="intersection-observer-settings">
-            <div class="uk-margin">
-              <div>
-                <label for="threshold">Threshold</label>
-              </div>
-              <input
-                class="uk-range"
-                type="range"
-                v-model="model.config.threshold"
-                min="0"
-                max="1"
-                step="0.1"
-              />
-              <span class="uk-range--label">{{ model.config.threshold }}</span>
-            </div>
-            <div class="uk-margin">
-              <label
-                ><input
-                  v-model="model.config.triggerOnce"
-                  class="uk-checkbox"
-                  type="checkbox"
-                />
-                Trigger once</label
-              >
-            </div>
-          </div>
-        </template>
-      </div>
-    </template>
+    Here is front of your plugin
   </div>
 </template>
 
@@ -122,36 +9,17 @@ export default {
   mixins: [window.Storyblok.plugin],
   data: function() {
     return {
-      moreOptions: false,
-      title: "fading plugin",
+      title: "Here is your data"
     };
   },
   methods: {
-    toggleMoreOptions() {
-      this.moreOptions = !this.moreOptions;
-    },
+    // declare methods like in normal vue component here 
     initWith() {
+      // init `model` with that values
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: "component-fade-plugin",
-        classNames: {
-          before: `before-fade`,
-          after: `after-fade`,
-        },
-        transitionConfig: {
-          duration: 500,
-          transitionProperty: "opacity", // HIDE
-          transitionTimingFunction: "ease", // HIDE
-          delay: 200,
-        },
-        config: {
-          triggerOnce: true, // HIDE
-          threshold: 0.3, // HIDE
-          // rootMargin: `0px 0px 0px 0px`
-        },
-        effect: "fade",
-        enabledOnMobile: true,
-        enabled: false,
+        plugin: "name-of-the-plugin",
+        data: "some data"
       };
     },
     pluginCreated() {
@@ -168,12 +36,6 @@ export default {
       },
       deep: true,
     },
-    // rootMargin: {
-    //   handler: function(value) {
-    //     console.log(value);
-    //     this.model.config.rootMargin = `${value[0]}px ${value[1]}px ${value[2]}px ${value[3]}px`
-    //   }
-    // }
   },
 };
 </script>
@@ -182,33 +44,5 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
-}
-
-.link:hover {
-  color: #09b3af;
-}
-
-.uk-range--label {
-  margin-left: 16px;
-}
-
-.intersection-observer-settings__heading {
-  margin: 16px 0 0 0;
-  padding: 0;
-}
-
-.intersection-observer-settings {
-  border: 1px outset #e9e9e9;
-  padding: 16px;
-}
-
-.transition-settings__heading {
-  margin: 16px 0 0 0;
-  padding: 0;
-}
-
-.transition-settings {
-  border: 1px outset #e9e9e9;
-  padding: 16px;
 }
 </style>
